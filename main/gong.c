e05da1d8ec76d399e8eb3c810f9476d16cb7d46d
zen_vega <ufogreeks@gmail.com>
2025-08-02 22:08:31 +0200

    feat: ip init

--- main/gong.c
+++ main/gong.c
@@ -40,7 +40,9 @@ static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_b
 		case IP_EVENT_STA_GOT_IP:
 		{
 			ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-    		printf("WIFI GOT IP: " IPSTR "\n", IP2STR(&event->ip_info.ip));
+			printf("IP Address: " IPSTR "\n", IP2STR(&event->ip_info.ip));
+			printf("Subnet Mask: " IPSTR "\n", IP2STR(&event->ip_info.netmask));
+			printf("Gateway (Router): " IPSTR "\n", IP2STR(&event->ip_info.gw));
 			break;
 		}
 		default:
@@ -50,11 +52,20 @@ static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_b
 	}
 }
 
+void	define_fixed_ip_adr(void)
+{
+	esp_netif_ip_info_t ip_info;
+
+	ip_info.ip.addr = esp_ip4addr_aton(CONFIG_WIFI_STATIC_IP);
+	ip_info.gw.addr = esp_ip4addr_aton(CONFIG_WIFI_GATEWAY);
+	ip_info.netmask.addr = esp_ip4addr_aton(CONFIG_SN_MASK);
+}
+
 void wifi_connection(void)
 {
 	esp_netif_init(); //Initialize the underlying TCP/IP stack.
 	esp_event_loop_create_default(); //responsible for creating and dispatching events
-	esp_netif_create_default_wifi_sta(); //creates structs for wifi-station
+	esp_netif_t *netif = esp_netif_create_default_wifi_sta(); //creates structs for wifi-station
 	wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT(); //sets up wifi wifi_init_config struct with default values
 	esp_wifi_init(&wifi_initiation);
 	esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL); //register wifi events
